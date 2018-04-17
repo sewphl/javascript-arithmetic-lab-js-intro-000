d5e3a9cfb7de2298c8b936f07bb40ce0c1dba9b7
sharon <sharonoptsout@gmail.com>
2018-04-17 00:38:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function add(a,b) {}
+
+function subtract(a,b) {}
+
+function multiply(a,b) {}
+
+function divide(a,b) {}
+
+function inc(n) {};
+
+function dec(n) {};
+
+function makeInt(n) {};
+
+function preserveDecimal(n) {};
